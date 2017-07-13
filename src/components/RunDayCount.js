36b5df132d83ba8c5c65e2cebd7269ad553bf4e3
Hervinho <herventubi@gmail.com>
2017-07-13 16:09:46 +0200

    Add react-icons

--- src/components/RunDayCount.js
+++ src/components/RunDayCount.js
@@ -1,4 +1,7 @@
 import '../stylesheets/ui.scss'
+import Terrain from 'react-icons/lib/md/terrain' //this is to get only ht eicons needed
+import Run from 'react-icons/lib/md/directions-run'
+import Calendar from 'react-icons/lib/fa/calendar'
 
 //methods to do calculations
 const percentToDecimal = (decimal) => ((decimal * 100) + '%')
@@ -12,14 +15,18 @@ export const RunDayCount = ({total, weather, location, goal}) => (//can use () i
     <div className="run-day-count">
             <div className="total-days">
                 <span>{total}</span>
+                    <Calendar />
                 <span> days</span>
             </div>
             <div className="rainy-days">
                 <span>{weather}</span>
+                    <Run />
                 <span> days</span>
             </div>
             <div className="sprint-days">
                 <span>{location}</span>
+                    <Terrain />
+                <span> </span>
             </div>
             <div className="sprint-days">
                 <span>{calculateGoalProgress(total, goal)}</span>
