58fa2c0395c27e9d12e71aa8a92e0c0cf7b86706
Hervinho <herventubi@gmail.com>
2017-07-13 14:33:48 +0200

    Initial commit

---
+++ src/components/RunDayCount.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import './../stylesheets/ui.scss'
+
+const RunDayCount = React.createClass({
+    
+})
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {render} from 'react-dom' //render property comes from React DOM
+
+//Render our React element.
+render( 
+    
+    <div>
+        
+    </div>,
+    document.getElementById('react-container') //where we wanna render the element
+)
