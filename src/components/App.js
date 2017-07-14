6a8ef5148f4b9578caa7493de6ea8eea28e91a0d
Hervinho <herventubi@gmail.com>
2017-07-14 16:33:27 +0200

    Refactor App.js with ES6 syntax

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,25 @@
-import {createClass} from 'react'
+import {Component} from 'react'
 import {RunDayList} from './RunDayList'
 import {RunDayCount} from './RunDayCount'
 
-export const App = createClass({
-    //initialize default state
-    getInitialState(){
-        return {
+export class App extends Component{
+    //initialize default state with ES6 class constructor
+    constructor(props){
+        super(props)
+        this.state = {
             allRunDays: [
                 {location: "Silverton", date: new Date("06/22/2015"), hill: true, street: "Pitts", rain: true},
                 {location: "Silverton", date: new Date("07/26/2016"), hill: false, street: "Cresswell", rain: false},
                 {location: "Silverton", date: new Date("09/30/2016"), hill: true, street: "Ripley", rain: false}
             ]
         }
-    },
+    }
+
     countDays(filter){
         const {allRunDays} = this.state
-        return allRunDays.filter(
-            // (if(filter) return day[filter]. else return day ).length
-            (day) => (filter) ? day[filter] : day).length
-    },
+        return allRunDays.filter((day) => (filter) ? day[filter] : day).length // (if(filter) return day[filter]. else return day ).length
+    }
+
     render(){
         return (
             <div className="app">
@@ -27,4 +28,4 @@ export const App = createClass({
             </div>
         )
     }
-})
+}
