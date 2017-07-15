15faa270445cc0a7c969cb7617c727a35400dee0
Hervinho <herventubi@gmail.com>
2017-07-15 10:52:22 +0200

    code cleanup

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {render} from 'react-dom' //render property comes from React DOM
+import {render} from 'react-dom' 
 import {RunDayList} from './components/RunDayList'
 import {RunDayCount} from './components/RunDayCount'
 import {App} from './components/App'
@@ -12,14 +12,6 @@ window.React = React//to avoid errors such as 'React is not defined'
 
 //Render our React element.
 render( 
-/*<HashRouter>
-        <div>
-            <Route path="/" component={App} />
-            <Route path="list" component={App} />
-            <Route path="add" component={AddDay} />
-            
-        </div>
-    </HashRouter>,*/
     routes,
-    document.getElementById('react-container') //where we wanna render the element
+    document.getElementById('react-container') 
 )
